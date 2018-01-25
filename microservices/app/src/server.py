e9d8d4cea980eec5b8426a3e3d44bbefbf2a7a43
unknown <christinethomas221@gmail.com>
2018-01-25 19:26:19 +0400

    fixing issue

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -61,6 +61,5 @@ def makeWebhookResult(req):
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
 
     app.run(debug=True, port=port, host='0.0.0.0')
